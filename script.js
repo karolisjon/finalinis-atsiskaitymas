99db192008c9278070bfac4884ca8bf94b3e11b9
unknown <karolisjonn@gmail.com>
2021-12-20 18:52:41 +0200

    pirmadienis v2

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 "use strict";
+
+// About section - tab navigation
+
+const tabNavigationElements = document.querySelectorAll(".about .tab-navigation li");
+
+function removeActiveClass(elements) {
+    for (let tab of tabNavigationElements) {
+        tab.classList.remove("active");
+    }
+}
+
+for (let tabEl of tabNavigationElements) {
+    tabEl.addEventListener("click", function() {
+        removeActiveClass(tabNavigationElements);
+        console.log('hi');
+        this.classList.add("active");
+    });
+}
+

47d29c45ac6354e2a9b5d2a57a2a1e016609549b
unknown <karolisjonn@gmail.com>
2021-12-27 11:39:00 +0200

    media queries v1

--- script.js
+++ script.js
@@ -83,5 +83,13 @@ const swiper = new Swiper('.swiper', {
 
   window.addEventListener("load", getCurrentWeather)
 
-
+// HAMBURGER 
+// Look for .hamburger
+var hamburger = document.querySelector(".hamburger");
+// On click
+hamburger.addEventListener("click", function() {
+  // Toggle class "is-active"
+  hamburger.classList.toggle("is-active");
+  // Do something else, like open/close menu
+});
 
